146adad954051d931090192e1778af0816097ae9
Александр Немеров <nemerov_a@mail.ru>
2022-05-11 03:54:34 +0800

    Добавьте файлы проекта.

---
+++ LabW_1_2_11_Progs/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Fisrt_General_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер строки: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите строку: ");
+            char[] arr = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToChar(Console.ReadLine());
+            }
+
+
+
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write("{0} ", arr[j]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
